72d455b0cb9d0630cb13bc1189993a389d07a7f7
gjovic1 <gjovic1@etf.unsa.ba>
2020-03-07 10:58:38 +0100

    Isprobane kontrole, sve radi očekivano.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Button btnBtn;
+    public Button clsBtn;
+    public Label chgLabel;
+    public void changeLabelDescription(ActionEvent actionEvent) {
+        chgLabel.setText("Dobar dan!");
+    }
+
+    public void closeWindow(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
